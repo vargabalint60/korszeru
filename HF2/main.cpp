8ee6b8a1eb91a79602bbbc2da82dc2f9ee483d77
vargabalint60 <74798347+vargabalint60@users.noreply.github.com>
2021-03-25 15:53:23 +0100

    Add files via upload

---
+++ HF2/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "vector2.h"
+
+int main()
+{
+    vector2d<double> v1;
+    std::cout << "Enter vector 1:" << std::endl;
+    std::cin >> v1;
+    std::cout << "v1 = " << v1 << std::endl;
+
+    vector2d<double> v2;
+    std::cout << "Enter vector 2:" << std::endl;
+    std::cin >> v2;
+    std::cout << "v2 = " << v2 << std::endl;
+    vector2d<double> v3 = v2 - v1;
+    std::cout << "v3 = v2-v1 = " << v3 << std::endl;
+
+    vector2d<double> v4;
+    std::cout << "v4 = " << v4 << std::endl;
+
+    v3 += v2;
+    std::cout << "v3 += v2 = " << v3 << std::endl;
+
+    v3 -= v2;
+    std::cout << "v3 -= v2 = " << v3 << std::endl;
+
+    v3 /= 3;
+    std::cout << "v3 /= 3 = " << v3 << std::endl;
+
+    v3 *= 3;
+    std::cout << "v3 *= 3 = " << v3 << std::endl;
+
+    vector2d<double> v5 = 3*v3;
+    std::cout << "v5 = 3*v3 = " << v5 << std::endl;
+
+    v5 = v3*3;
+    std::cout << "v5 = v3*3 = " << v5 << std::endl;
+
+    double a = dot(v3,v2);
+    std::cout << "dot(v3,v2) = " << a << std::endl;
+
+    double l = length(v3);
+    std::cout << "length(v3) = " << l << std::endl;
+
+    double sql = sqlength(v3);
+    std::cout << "sqlength(v3) = " << sql << std::endl;
+
+    v4 = normalize(v3);
+    std::cout << "v4 = normalize(v3) = " << v4 << std::endl;
+
+    bool eq = v3 == v4;
+    std::cout << "v3 == v4 = " << eq << std::endl;
+
+    bool eq2 = v3 == v3;
+    std::cout << "v3 == v3 = " << eq2 << std::endl;
+
+    bool neq = v3 != v4;
+    std::cout << "v3 != v4 = " << neq << std::endl;
+
+    bool neq2 = v3 != v3;
+    std::cout << "v3 != v3 = " << neq2 << std::endl;
+
+    return 0;
+}
---
+++ HF2/vector2.h
@@ -0,0 +1,92 @@
+#include <cmath>
+
+template<typename T>
+class vector2d
+{
+public:
+    T x,y;
+    vector2d(std::initializer_list<T> a) : x{*a.begin()},y{*(a.end()-1)}{}
+    vector2d() : x{0},y{0}{}
+
+    vector2d<T> operator+=(vector2d<T> const& b){
+        x += b.x;
+        y += b.y;
+        return *this;
+    }
+
+    vector2d<T> operator+(vector2d<T> const& b){
+        return vector2d{x+b.x, y+b.y};
+    }
+
+    vector2d<T> operator-=(vector2d<T> const& b){
+        x -= b.x;
+        y -= b.y;
+        return *this;
+    }
+
+    vector2d<T> operator-(vector2d<T> const& b){
+        return vector2d{x-b.x, y-b.y};
+    }
+
+    vector2d<T> operator*=(T const& a){
+        x = x*a;
+        y = y*a;
+        return *this;
+    }
+
+    template<typename A>
+    vector2d<T> operator*(A const& a){
+        return vector2d{a*x, a*y};
+    }
+
+    vector2d<T> operator/=(T const& a){
+        x = x/a;
+        y = y/a;
+        return *this;
+    }
+
+    friend bool operator==(vector2d<T> const& a,vector2d<T> const& b){
+        if (a.x == b.x and a.y == b.y){return true;}
+        else{return false;}
+    }
+
+    friend bool operator!=(vector2d<T> const& a,vector2d<T> const& b){
+        if (a.x == b.x and a.y == b.y){return false;}
+        else{return true;}
+    }
+
+    friend std::ostream& operator<<(std::ostream& o, vector2d<T>const& v){
+        o << "[" << v.x << ", " << v.y << "]";
+        return o;
+    }
+
+    friend std::istream& operator>>(std::istream& i, vector2d<T>& v){
+        i >> v.x;
+        i >> v.y;
+        return i;
+    }
+
+    template< typename A>
+    friend vector2d<T> operator*(A const& a, vector2d<T> const& v){
+        return vector2d{a*v.x, a*v.y};
+    }
+
+    friend vector2d<T> normalize(vector2d<T> const& v){
+        return vector2d{v.x/length(v), v.y/length(v)};
+    }
+};
+
+template<typename T>
+T dot(vector2d<T> const& a, vector2d<T> const& b){
+    return a.x*b.x + a.y*b.y;
+}
+
+template<typename T>
+T length(vector2d<T> const& v){
+    return sqrt(v.x*v.x + v.y*v.y);
+}
+
+template<typename T>
+T sqlength(vector2d<T> const& v){
+    return v.x*v.x + v.y*v.y;
+}
