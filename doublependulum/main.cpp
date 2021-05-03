0f914dcbf71700a9023089ffb0785ed0d866c883
vargabalint60 <74798347+vargabalint60@users.noreply.github.com>
2021-05-03 14:00:56 +0200

    Add files via upload

--- doublependulum/main.cpp
+++ doublependulum/main.cpp
@@ -7,27 +7,24 @@
 
 
 double const g = 9.81;
-std::vector<double> k1(4);
+std::vector<double> k1(4);   //a Runge-Kuttához szükséges k változók
 std::vector<double> k2(4);
 std::vector<double> k3(4);
 std::vector<double> k4(4);
-std::vector<double> k5(4);
-std::vector<double> k6(4);
-
 
 class pendulum{
 public:
-    double m1 = 1;
-    double m2 = 1;
-    double l1 = 90;
-    double l2 = 90;
-    double phi1 = -0.4;
-    double phi2 = -0.4;
+    double m1 = 1.5753;    //kezdeti feltételek
+    double m2 = 0.5753;
+    double l1 = 0.5*200;
+    double l2 = 0.5*200;
+    double phi1 = M_PI/2-0.3;
+    double phi2 = -M_PI/4;
     double dphi1 = 0;
     double dphi2 = 0;
     double h = 0.05;
 
-    double ddphi1(double x1, double x2, double dx1, double dx2){
+    double ddphi1(double x1, double x2, double dx1, double dx2){       //a második deriváltakat kiértékelő függvények
         return (m2*l1*dx1*dx1*sin(x2-x1)*cos(x2-x1) + m2*g*sin(x2)*cos(x2-x1) + m2*l2*dx2*dx2*sin(x2-x1) -
                 (m1+m2)*g*sin(x1))/((m1+m2)*l1 - l1*cos(x2-x1)*cos(x2-x1));}
 
@@ -42,7 +39,7 @@ public:
         phi2 += h * dphi2;
     }
 
-    void RK4(){
+    void RK4(){     //a jegyzőkönyvben leírt módon a Runge-Kutta
         k1[0] = dphi1;
         k1[1] = ddphi1(phi1,phi2,dphi1,dphi2);
         k1[2] = dphi2;
@@ -68,7 +65,7 @@ public:
         phi2 += h/6*(k1[2] + 2*k2[2] + 2*k3[2] + k4[2]);
         dphi2 += h/6*(k1[3] + 2*k2[3] + 2*k3[3] + k4[3]);
 
-        if (phi1 > M_PI){phi1-=2*M_PI;}
+        if (phi1 > M_PI){phi1-=2*M_PI;}    //a fázistér miatt, hogyha átfordul az inga, akkor nem növeli tovább a phi értékét
         if (phi1 < -M_PI){phi1+=2*M_PI;}
         if (phi2 > M_PI){phi2-=2*M_PI;}
         if (phi2 < -M_PI){phi2+=2*M_PI;}
@@ -78,20 +75,20 @@ public:
 
 int main() {
     int x = 0, y = 0;
-    double mphi1 = 0, mphi2 = 0, mdphi1 = 0, mphi3 = 0, mphi4 = 0, mdphi2=0;
-    bool a=false, b=false;
-    vector2d<double> state;
+    double mphi1 = 0, mphi2 = 0, mdphi1 = 0, mdphi2=0;  //az egér és a két inga által bezárt szögek, és szögsebességek
+    bool a=false, b=false;                              //ezekben lesz tárolva, hogy le van e nyomva az egér
 
     pendulum pend;
-    std::vector<std::vector<vector2d<double>>> data{{{pend.phi1,pend.dphi1},{pend.phi2,pend.dphi2}}};
-    std::vector<vector2d<double>> data2;
+    std::vector<std::vector<vector2d<double>>> data{{{pend.phi1,pend.dphi1},{pend.phi2,pend.dphi2}}};  //minden lefutáskor ebbe írja a szögeket ésa szögsebességeket
+                                                                                                        //a fázistér miatt van rá szükség
     MainWindow wnd;
     wnd.window.eventDriven = false;
     wnd.mouseHandler([&](Mouse const& m)
                      {
                          x = m.x; y = m.y;
-                         mphi3 = mphi1;
-                         mphi4 = mphi2;
+                         //a következő sorok az egér és az ingák közötti szöget számítják ki, és a szögsebességet határozzák meg,
+                         //amivel elinduljon az inga ha elengedik
+                         //több esetre azért van szükség, mivel az előjelek miatt anélkül nem tudná megkülönböztetni például a bal felső és a jobb alsó térfelet
                          if (y < 200){
                              if (x > 320){mphi1 = atan((double)(x-320)/(y-200))+M_PI;}
                              if (x < 320){mphi1 = atan((double)(x-320)/(y-200))-M_PI;}
@@ -106,13 +103,14 @@ int main() {
                          else{mphi2 = atan((x-320-pend.l1*sin(pend.phi1))/(y-200-pend.l1*cos(pend.phi1)));}
                          mdphi2 = pend.phi2-mphi2;
 
-
+                         //érzékeli az egér lenyomását, és az a-t vagy  b-t true-val teszi egyeblőé, attól függően, hogy melyik ingához van közlebb az egér
                          if(m.event == Mouse::LeftDown  ){
                              if (pow(320+pend.l1*sin(pend.phi1)-x,2)+pow(200+pend.l1*cos(pend.phi1)-y,2)<
                                 pow(320+pend.l1*sin(pend.phi1)+pend.l2*sin(pend.phi2)-x,2)+pow(200+pend.l1*cos(pend.phi1)+pend.l2*cos(pend.phi2)-y,2)){
                                  a = true;
                              }
                              else{b = true;}}
+                         //érzékeli az egér elengedését, visszaállítja az a-t és a b-t és beállítja a sebességet
                          else if(m.event == Mouse::LeftUp    ){
                              if(b){pend.dphi2 = -10*mdphi2;}
                              if(a){pend.dphi1 = -10*mdphi1;}
@@ -123,6 +121,8 @@ int main() {
 
     wnd.resizeHandler([&](int w, int h, StateChange sc){ } );
     wnd.idleHandler([&]{
+
+        //ha az a vagy a b true akkor azokat irányítja az egér, ha nem, futtatja az RK-t
         if (b){
             pend.phi2 = mphi2;
             pend.dphi1 = 0;
@@ -140,17 +140,21 @@ int main() {
 
         r.forall_pixels([](auto, auto, auto){ return color(255, 255, 255); });
 
+        //a két inga ábrázolása
         r.ellipse(int(320),int(200),5,5,[](auto){ return color(100, 100, 100); });
         r.line(320,200,int(320+pend.l1*sin(pend.phi1)),int(200+pend.l1*cos(pend.phi1)),[](auto){ return color(100, 100, 100); });
         r.ellipse(int(320+pend.l1*sin(pend.phi1)),int(200+pend.l1*cos(pend.phi1)),5,5,[](auto){ return color(0, 200, 100); });
         r.line(int(320+pend.l1*sin(pend.phi1)),int(200+pend.l1*cos(pend.phi1)),int(320+pend.l1*sin(pend.phi1)+pend.l2*sin(pend.phi2)),int(200+pend.l1*cos(pend.phi1)+pend.l2*cos(pend.phi2)),[](auto){ return color(100, 100, 100); });
         r.ellipse(int(320+pend.l1*sin(pend.phi1)+pend.l2*sin(pend.phi2)),int(200+pend.l1*cos(pend.phi1)+pend.l2*cos(pend.phi2)),5,5,[](auto){ return color(255, 0, 0); });
 
+
+        //fázistér
         for (auto e : data){
             r.setpixel(900+(int)100*e[0].x,200+(int)100*e[0].y,color(0, 200, 100));
             r.setpixel(900+(int)100*e[1].x,200+(int)100*e[1].y,color(255, 0, 0));
         }
 
+        //koordináta rendszer
         r.line(580,200,1220,200,[](auto){ return color(100, 100, 100); });
         r.line(900,10,900,390,[](auto){ return color(100, 100, 100); });
         r.line(585,205,585,195,[](auto){ return color(100, 100, 100); });
